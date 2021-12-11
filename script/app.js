1c3ab0626cc7ff73ba9095a51906421e1c4ad07f
Sarahars <arousarah@gmail.com>
2021-12-11 11:14:21 +0000

    script

--- script/app.js
+++ script/app.js
@@ -1,1 +1,22 @@
  
+    
+    var a = 5;
+    a = "Sarah";
+    console.log(a);
+
+    var b = 3;
+    var c = 8;
+    var result = a + " " + b;
+    console.log(result + c);
+
+    var a = "Sarah";
+    var b = "Javascript";
+    console.log(a + b);
+
+
+
+    function somme(a, b) {
+      var c = a + b;
+      console.log(c);
+    }
+    somme(86, 29);
